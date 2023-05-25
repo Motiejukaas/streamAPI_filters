7b64212e4e27aad9c2b085a662466b716ee129d8
Motiejus Šveikauskas <118689022+Motiejukaas@users.noreply.github.com>
2023-05-25 22:00:34 +0300

    Final polishes

--- src/main/java/com/streamapi_filters/FilterApplication.java
+++ src/main/java/com/streamapi_filters/FilterApplication.java
@@ -7,6 +7,8 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+//@author Motiejus Šveikauskas
+
 public class FilterApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
